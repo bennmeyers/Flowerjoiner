1ddf298dbeddadc3bb99abff14c17c36cb849b57
Benjamin Meyers <benjamin.meyers@dermveda.com>
2016-03-11 17:24:29 -0800

    Added and tweaked the portfolio page to a stable point.

--- assets/includes/global-header.php
+++ assets/includes/global-header.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-    <title></title>
+    <title>Benjamin Meyers • Portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css'>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
     <link href="assets/css/custom.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 <body>
