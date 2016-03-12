55b1319867e45318dde56f7e129f96ef72f7568e
Benjamin Meyers <benjamin.meyers@dermveda.com>
2016-03-11 23:02:31 -0800

    restructured portfolio includes

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 			var lsDescription = $(this).find("description").text(); // put the description in a variable
 			var lsThumb = $(this).find("thumbnail").text(); // put the thumbnail in a variable
 			var lsID = lsSection.toLowerCase(); // reformat the string to be lower case
-			var lsProject = '<figure class="col-sm-4" id="' + lsThumb + '"><p><a href="/assets/img/samples/full/' + lsThumb + '.jpg" title="' + lsWebsite + '" class="lightbox" target="_blank"><img src="/assets/img/samples/preview/' + lsThumb + '.png" alt="' + lsWebsite + '" /></a></p><figcaption class="description"><strong><a href="' + lsLink + '" title="' + lsWebsite + '" rel="external">' + lsWebsite + '</a> ' + lsSection + '</strong> '; // define the Local String for the Project
+			var lsProject = '<figure class="col-sm-4" id="' + lsThumb + '"><a href="/assets/img/samples/full/' + lsThumb + '.jpg" title="' + lsWebsite + '" class="lightbox" target="_blank"><img src="/assets/img/samples/preview/' + lsThumb + '.png" alt="' + lsWebsite + '"></a><figcaption class="description"><strong><a href="' + lsLink + '" title="' + lsWebsite + '" rel="external">' + lsWebsite + '</a> ' + lsSection + '</strong> '; // define the Local String for the Project
 
 			lsProject += (lsDemoLink) ? '' : ''; // if there is a demo link add it to the Local String for the Project
 			lsProject += lsDescription + '</figcaption></figure><!-- #' + lsThumb + ' -->'; // define the Local String for the Project
@@ -18,10 +18,8 @@ $(document).ready(function(){
 
 		$('.lightbox').fancybox(); // call the lightbox function : needs to be within the "get" tag so it fires at the correct time
 
-		$('.design_example:nth-child(3n)').addClass('last'); // put the 'last' class on every third design_example : needs to be within the "get" tag so it fires at the correct time
-		$('.design_example:nth-child(3n+1)').addClass('first'); // put the 'first' class on every fourth design_example : needs to be within the "get" tag so it fires at the correct time
-		$('.design_example:first-child').addClass('first'); // put the 'first' class on the first design_example : needs to be within the "get" tag so it fires at the correct time
-		$('.design_example p:first-child').addClass('first'); // put the 'first' class on the first p tag in a design_example : needs to be within the "get" tag so it fires at the correct time
-		$('.design_example p:last-child').addClass('last'); // put the 'last' class on the last p tag in a design_example : needs to be within the "get" tag so it fires at the correct time
+		$('#professional figure:nth-child(3n)').addClass('last'); // put the 'last' class on every third design_example : needs to be within the "get" tag so it fires at the correct time
+		$('#professional figure:nth-child(3n+1)').addClass('first'); // put the 'first' class on every fourth design_example : needs to be within the "get" tag so it fires at the correct time
+		$('#professional figure:first-child').addClass('first'); // put the 'first' class on the first design_example : needs to be within the "get" tag so it fires at the correct time
 	}); // jQuery Get
 });
