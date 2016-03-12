8941ec755363ec27b0cb1e2cb30e5fe16e539906
Benjamin Meyers <benjamin.meyers@dermveda.com>
2016-03-11 18:14:58 -0800

    portfolio.js modifications
    made changes to alt tag and removed dead code

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -8,14 +8,10 @@ $(document).ready(function(){
 			var lsDescription = $(this).find("description").text(); // put the description in a variable
 			var lsThumb = $(this).find("thumbnail").text(); // put the thumbnail in a variable
 			var lsID = lsSection.toLowerCase(); // reformat the string to be lower case
-			var lsProject = '<div class="col-sm-4"><p><a href="/assets/img/samples/full/' + lsThumb + '.jpg" title="' + lsSection + '" class="lightbox" target="_blank"><img src="/assets/img/samples/preview/' + lsThumb + '.png" alt="' + lsSection + '" /></a></p><p class="description"><strong><a href="' + lsLink + '" title="' + lsWebsite + '" rel="external">' + lsWebsite + '</a> ' + lsSection + '</strong> '; // define the Local String for the Project
+			var lsProject = '<figure class="col-sm-4" id="' + lsThumb + '"><p><a href="/assets/img/samples/full/' + lsThumb + '.jpg" title="' + lsWebsite + '" class="lightbox" target="_blank"><img src="/assets/img/samples/preview/' + lsThumb + '.png" alt="' + lsWebsite + '" /></a></p><figcaption class="description"><strong><a href="' + lsLink + '" title="' + lsWebsite + '" rel="external">' + lsWebsite + '</a> ' + lsSection + '</strong> '; // define the Local String for the Project
 
 			lsProject += (lsDemoLink) ? '' : ''; // if there is a demo link add it to the Local String for the Project
-			lsProject += lsDescription + '</p></div><!-- #' + lsID + ' -->'; // define the Local String for the Project
-			
-			lsID = lsID.replace(".",""); // reformat the string to remove periods
-			lsID = lsID.replace(/\//g,"_"); // reformat the string to remove slashes
-			lsID = lsID.replace(/ /g,"_"); // reformat the string to remove spaces
+			lsProject += lsDescription + '</figcaption></figure><!-- #' + lsThumb + ' -->'; // define the Local String for the Project
 
 			$('#professional').append(lsProject); // append the projects together
 		}); // each
