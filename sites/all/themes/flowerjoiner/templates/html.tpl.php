3d6c2e088677bf1451b5192635296cb1a7558433
Benjamin Meyers <benjamin@flowerjoiner.com>
2016-10-13 19:48:42 -0700

    created git backup of sites directory

---
+++ sites/all/themes/flowerjoiner/templates/html.tpl.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes ?>><![endif]-->
+<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes ?>><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes ?>><![endif]-->
+<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes ?>><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html<?php print $html_attributes . $rdf_namespaces ?>><!--<![endif]-->
+
+<head><!-- fj7-4.x -->
+  <title><?php print $head_title ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robot" content="index,follow">
+  <meta name="keywords" content=".NET, AJAX, Analytics, AngularJS, ASP, CSS, Drupal, Eclipse, Git, HTML, JavaScript, jQuery, KnockoutJS, MySQL, OS X, Perl, Photoshop, PHP, Python, SEO, XML, WordPress">
+  <?php print $head ?>
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,300italic,700' rel='stylesheet' type='text/css'>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+  <link href="/assets/css/custom.css" rel="stylesheet" type="text/css" media="screen">
+  <?php print $styles ?>
+</head>
+
+<body id="<?php print $body_id ?>" class="<?php print $classes ?>" <?php print $attributes ?>>
+<?php print $page_top ?>
+<?php print $page ?>
+<?php print $page_bottom ?>
+
+<?php print $scripts ?>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.1.min.js" integrity="sha256-gvQgAFzTH6trSrAWoH1iPo9Xc96QxSZ3feW6kem+O00=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/assets/js/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="/assets/js/custom-shared.js"></script>
+<script type="text/javascript" src="/assets/js/portfolio.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-11744746-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ sites/all/themes/flowerjoiner/templates/page.tpl.php
@@ -0,0 +1,70 @@
+<div id="page-homepage" class="container">
+  <div id="skip-link">
+    <a href="#main" class="element-invisible element-focusable"><?php print t('Skip to main content') ?></a>
+  </div>
+  <header class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <svg height="40" width="40">
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#727E9C;" transform="translate(20 35) rotate(90)" /><!-- north -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#727E9C;" transform="translate(20 5) rotate(90)" /><!-- south -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#727E9C;" transform="translate(5 20)" /><!-- west -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#727E9C;" transform="translate(35 20)" /><!-- east -->
+
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#97BAD9;" transform="translate(9.125 9.125) rotate(45)" /><!-- north-west -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#97BAD9;" transform="translate(30.875 30.875) rotate(45)" /><!-- south-east -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#97BAD9;" transform="translate(30.875 9.125) rotate(-45)" /><!-- north-east -->
+          <ellipse cx="0" cy="0" rx="5" ry="2.5" style="fill:#97BAD9;" transform="translate(9.125 30.875) rotate(-45)" /><!-- south-west -->
+
+          <circle cx="0" cy="0" r="5" style="fill:#A6748A;" transform="translate(20 20)" />
+          s
+        </svg> 
+      </a>
+      <nav class="navbar-header">
+        <?php if ($main_menu): ?>
+          <?php print theme('links__system_main_menu', array('links' => menu_tree(variable_get('menu_main_links_source', 'main-menu')))); ?>
+        <?php endif; ?>
+      </nav>
+    </div>
+  </header>
+  <div id="content" class="row">
+    <div id="main_content" role="main" class="col-md-9">
+      <div class="row">
+        <?php if ($page['highlighted']): ?><div id="mission"><?php print render ($page['highlighted']); ?></div><?php endif; ?>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): if ($is_front){ print '<h2 class="title">'. $title .'</h2>'; } else { print '<h1 class="title">'. $title .'</h1>'; } endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+      <div class="row">
+        <div class="tabs"><?php print render($tabs); ?></div>
+      </div>
+      <div class="row">
+        <?php print render($page['help']); ?>
+        <?php print $messages ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php if ($page['content']) : ?><?php print render ($page['content']); ?><?php endif; ?>
+        <?php print $feed_icons; ?>
+      </div>
+    </div><!-- #main_content -->
+    <aside id="sidebar" class="col-md-3" role="complementary">
+      <a href="https://secure.nationalmssociety.org/site/Donation2?df_id=55896&PROXY_ID=8643532&PROXY_TYPE=20&FR_ID=27080&s_src=BF_emailbadge&_ga=1.131583306.43404973.1464564988" target="new" class="btn btn-primary col-md-12">Fight MS with me</a>
+      <br>
+      <br>
+      <a href="https://www.learngreenflower.com/courses/10/cannabis-treat-substance-dependence-addiction-amanda-reiman" target="new" class="btn btn-primary col-md-12" title="Green Flower Media">Treating Substance Addiction</a>
+    </aside><!-- #sidebar -->
+  </div><!-- #content -->
+</div><!-- #homepage -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="<?php print resp_class(); ?>1-5"><?php if (theme_get_setting('social_links_display')): ?><div id="soclinks"><?php print social_links(); ?></div><?php endif; ?></div>
+        <div class="<?php print resp_class(); ?>3-5"><?php if ($page['footer']): ?><?php print render ($page['footer']); ?><?php endif; ?></div>
+        <div class="<?php print resp_class(); ?>1-5"></div>
+        <br>
+        <br>
+        <a href="http://main.nationalmssociety.org/site/TR?px=8643532&pg=personal&fr_id=27080&s_src=BF_emailbadge" target="new" title="Fundraising Status"><img style="border:0" src="http://bfapps1.boundlessfundraising.com/badge/nmss/display/8643532/27080/status.jpg" alt="Fundraising Status" /></a>
+      </div>
+    </div>
+  </div>
+</footer>
